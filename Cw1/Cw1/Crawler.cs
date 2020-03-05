69594f454568f95d0ea22f9e662c857581770758
s18534 <s18534@LAB220-10>
2020-03-05 15:29:48 +0100

    Pierwszy commit

---
+++ Cw1/Cw1/Crawler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    class Crawler
+    {
+        string str;
+        public Crawler()
+        {
+            var url = "https://www.pja.edu.pl/";
+            str = url;
+        }
+
+        public async Task dom()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(str);
+
+            var htmlContent = await response.Content.ReadAsStringAsync();
+
+            var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+            var matches = regex.Matches(htmlContent);
+
+            foreach(var match in matches)
+            {
+                Console.WriteLine(match.ToString());
+            }
+
+        }
+    }
+}
---
+++ Cw1/Cw1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Cw1
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+
+            var op = new Crawler();
+            Console.WriteLine("bob");
+            op.dom();
+            Console.WriteLine("bob");
+            /*HttpClient client = new HttpClient();
+            string urlAddress = "https://4programmers.net/Forum/Off-Topic/251111-pomysl_na_nazwe_adresu_e";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+                if (response.CharacterSet == null)
+                    readStream = new StreamReader(receiveStream);
+                else
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                string data = readStream.ReadToEnd();
+                response.Close();
+                readStream.Close();
+                Regex emailRegex = new Regex(@"(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
+                 + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+                 + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                 + @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})",
+                RegexOptions.IgnoreCase);
+                //find items that matches with our pattern
+                MatchCollection emailMatches = emailRegex.Matches(data);
+
+                StringBuilder sb = new StringBuilder();
+
+                foreach (Match emailMatch in emailMatches)
+                {
+                    sb.AppendLine(emailMatch.Value);
+                }
+                Console.WriteLine(sb);
+            }*/
+        }
+
+    }
+}
